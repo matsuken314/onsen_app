9cdc6dda37041850eb816599dce51c395357ff0d
matsuken314 <kenei031455@gmail.com>
2021-10-31 22:50:35 +0900

    ゲストログイン機能の実装

---
+++ app/controllers/users/passwords_controller.rb
@@ -0,0 +1,7 @@
+class Users::PasswordsController < Devise::PasswordsController
+  before_action :ensure_normal_user, only: :create
+
+  def ensure_normal_user
+    redirect_to new_user_session_path, alert: "ゲストユーザーのパスワード再設定はできません。" if params[:user][:email].downcase == "guest@example.com"
+  end
+end
