5ef4b2af9f5577c3110c1320174a146bd3baf593
matsuken314 <kenei031455@gmail.com>
2021-11-08 06:50:36 +0900

    アップロードした温泉の画像はコミットしないようにする

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
+    @post = Post.new(post_params)
   end
 
   private
 
   def post_params
-    params.require(:post).permit
+    params.require(:post).permit(:totonoi_address, :sauna_one, :water_one, :totonoi_one, :sauna_two, :water_two, :totonoi_two, :sauna_three, :water_three,
+                                 :totonoi_three).merge(user_id: current_user.id)
   end
 end
