721f0d4d6eed9778fff0368b823e378115f22545
matsuken314 <kenei031455@gmail.com>
2021-11-21 17:50:40 +0900

    プロフ画像の更新可能へ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = current_user.posts
   end
+
+  def update
+    if @user.update(user_params)
+      redirect_to controller: :users, action: :show
+    else
+      render :edit
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :avatar)
+  end
 end
