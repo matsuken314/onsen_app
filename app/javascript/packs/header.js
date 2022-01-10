f3e0afa2ad441c835ae661f6386ab27534e8999b
matsuken314 <kenei031455@gmail.com>
2022-01-10 17:09:14 +0900

    トグルメニューの閉じる処理以外は実装

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -3,10 +3,6 @@ $(document).on('turbolinks:load', function() {
       $(".clickslide").click(function(){
         $("#dropdown__header").addClass('dropdown__active');
         console.log('開く？')
-        if( $('#dropdown__header').hasClass('dropdown__active')) {
-           $("#dropdown__header").removeClass('dropdown__active');
-            console.log('条件式は？')
-          }
       })
      
 
