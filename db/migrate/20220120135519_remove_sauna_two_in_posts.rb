57a62373a45b1655df3793bf265744eff879b364
matsuken314 <kenei031455@gmail.com>
2022-01-20 23:55:26 +0900

    Postテーブルにユーザーが言った温泉の温泉テーブルのidを格納するためのwent_onsen_idカラムを作成

---
+++ db/migrate/20220120135519_remove_sauna_two_in_posts.rb
@@ -0,0 +1,10 @@
+class RemoveSaunaTwoInPosts < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :posts, :sauna_two, :integer
+    remove_column :posts, :sauna_three, :integer
+    remove_column :posts, :water_two, :integer
+    remove_column :posts, :water_three,  :integer
+    remove_column :posts, :water_three,  :integer
+    remove_column :posts, :water_three,  :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_19_074711) do
+ActiveRecord::Schema.define(version: 2022_01_20_140604) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_12_19_074711) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "went_onsen_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
