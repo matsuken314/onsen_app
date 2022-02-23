37702fb1a9fa62e664be2230959b6ac5dc7250d1
matsuken314 <kenei031455@gmail.com>
2022-02-23 15:46:35 +0900

    第1段階ファーストビュー実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,7 @@
 class HomesController < ApplicationController
-  # before_action :authenticate_user!
-  # skip_before_action :authenticate_user!, only: %i[index]
+
   def index
-    @onsens = Onsen.all
+    @onsens = Onsen.order('id DESC').limit(6)
     @posts = Post.all
   end
 end
