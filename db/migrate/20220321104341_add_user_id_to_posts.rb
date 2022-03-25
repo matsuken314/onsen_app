3dbacbbc2ab391ae1f61a404317fadc02e970791
matsuken314 <kenei031455@gmail.com>
2022-03-25 19:42:13 +0900

    デプロイ時にエラーになっていたマイグレーションファイルを削除

--- db/migrate/20220321104341_add_user_id_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToPosts < ActiveRecord::Migration[6.1]
-  def change
-    add_foreign_key :posts, :users
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_104341) do
+ActiveRecord::Schema.define(version: 2022_03_08_132227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
