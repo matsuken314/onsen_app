076f7057694c48652c9a83e3d10f798b27b81bb0
matsuken314 <kenei031455@gmail.com>
2022-03-29 21:56:31 +0900

    画像つき投稿の設定

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,14 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.storage :fog
+    config.fog_provider = "fog/aws"
+    config.fog_directory = "totonoi-and-infra"
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, :region)
+      provider: "AWS",
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: ENV["AWS_DEFAULT_REGION"],
+      path_style: true
     }
-    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
   end
 end
